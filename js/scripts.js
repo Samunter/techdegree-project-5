8f94e2968e610197faab777bbbfe25d2e185a2f3
Samma <46258134+Samunter@users.noreply.github.com>
2019-11-26 22:14:40 -0800

    created event listener but it's broken

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+let people;
+
+function createGallery(people) {
+  let galleryHTML = '';
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+    galleryHTML += `
+        <div class="card" data-index=${i}>
+            <div class="card-img-container">
+                <img class="card-img" src="${person.picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="${person.name.first}-${person.name.last}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="card-text">${person.email}</p>
+                <p class="card-text cap">${person.location.city}</p>
+            </div>
+        </div>
+    `;
+  }
+  gallery.innerHTML = galleryHTML;
+}
+
+function formatDOB(person) {
+  const dob = person.dob.date;
+  const regex = /(\d{4})-(\d{2})-(\d{2}).*/;
+  const replacement = '$2/$3/$1';
+  return dob.replace(regex, replacement);
+}
+
+function createModal(people, index) {
+  const person = people[index];
+  const modalContainer = document.createElement('div');
+  document.body.appendChild(modalContainer);
+  modalContainer.className = 'modal-container';
+  modalContainer.innerHTML = `
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${
+                  person.picture.large
+                }" alt="profile picture">
+                <h3 id="${person.name.first}-${
+    person.name.last
+  }-modal" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">${person.cell}</p>
+                <p class="modal-text">${person.location.street.number} ${
+    person.location.street.name
+  }, ${person.location.city}, ${person.location.state} ${
+    person.location.postcode
+  }</p>
+                <p class="modal-text">Birthday: ${formatDOB(person)}</p>
+            </div>
+        </div>
+    `;
+}
+
+// dob:
+//  age: 68
+//  date: "1951-02-08T08:11:08.695Z"
+
+fetch(
+  'https://randomuser.me/api/?results=15&inc=name,email,location,dob,picture,cell&nat=us&noinfo'
+)
+  .then(res => res.json())
+  //.then(console.log);
+  .then(json => {
+    people = json.results;
+    createGallery(people);
+  });
+
+const cards = document.getElementsByClassName('card');
+for (let i = 0; i < cards.length; i++) {
+  const card = cards[i];
+  card.addEventListener('click', () => createModal(people, card.dataset.index));
+}
